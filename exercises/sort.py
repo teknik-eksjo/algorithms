7eac6451c5cb60f446d9f76b67a744042950e236
Oskar Svahn <eoskava0012utb.hoglandet.se>
2018-02-19 09:50:30 +0100

    Lösnigar sort och search

--- exercises/sort.py
+++ exercises/sort.py
@@ -4,6 +4,9 @@ Fler alternativ finns beskrivna på Wikipedia_.
 
 .. _Wikipedia: https://en.wikipedia.org/wiki/Sorting_algorithm#Popular_sorting_algorithms
 """
+import logging
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger()
 
 
 def bubble_sort(l):
@@ -14,8 +17,22 @@ def bubble_sort(l):
 
     .. _Wikipedia: https://en.wikipedia.org/wiki/Sorting_algorithm#Popular_sorting_algorithms
     """
-    return l
+    # x=True
+    # while x==True:
+    #     for i in range(len(l)-1, 0 ,-1):
+    #         x=False
+    #         if l[i]> l[i+1]:
+    #             l[i], l[i+1] = l[i+1], l[i]  # Swap
+    #             x=True
+    #
+    # return l
 
+
+    for i in range(len(l)-1, 0, -1):
+        for j in range(i):
+            if l[j] > l[j+1]:
+                l[j], l[j+1] = l[j+1], l[j]  # Byt plats
+    return l
 
 def insertion_sort(l):
     """Insertion sort.
@@ -25,6 +42,26 @@ def insertion_sort(l):
     .. _Wikipedia: https://en.wikipedia.org/wiki/Insertion_sort
 
     """
+    # x= True
+    # for i in range(0, len(l)-1):
+    #     while x == True:
+    #         if l[i]> l[i+1]:
+    #             l[i], l[i+1] = l[i+1], l[i]
+    #             i -=1
+    #             if i==1:
+    #                 x=False
+    # return l
+
+    logger.debug('Starting sort of list \n{}'.format(l))
+    for i in range(1, len(l)):
+        logger.debug('Iteration starts with list \n{}'.format(l))
+        j = i
+        logger.debug('Comparing {} (i) and {} (j).'.format(i, j))
+        while j > 0 and l[j-1] > l[i]:
+            j -= 1
+        logger.debug('Swapping index {} (i) and {} (j).'.format(i, j))
+        l = l[:j] + [l[i]] + l[j:i] + l[i+1:]
+
     return l
 
 
@@ -33,9 +70,20 @@ def selection_sort(l):
 
     Implementation av selection sort. Beskrivning finns på Wikipedia_.
 
-    .. _Wikipedia: https://en.wikipedia.org/wiki/Selection_sort
+.. _Wikipedia: https://en.wikipedia.org/wiki/Selection_sort
 
     """
+#    for i in range(1, len(l), -1):
+#        for j in range(i):
+#            k = j[l]
+
+
+    for i in range(len(l)):
+        minst = i
+        for n in range(i+1, len(l)):
+            if l[minst] > l[n]:
+                minst = n
+        l[i], l[minst] = l[minst], l[i]
     return l
 
 
@@ -48,4 +96,36 @@ def merge_sort(l):
     .. _Wikipedia: https://en.wikipedia.org/wiki/Sorting_algorithm#Popular_sorting_algorithms
 
     """
-    return l
+    length = len(l)
+    if length == 1:
+        return l
+
+    pivot = int(length/2)
+    left = merge_sort(l[:pivot])
+    right = merge_sort(l[pivot:])
+
+    result = []
+    while True:
+        if len(left) == 0:
+            result.extend(right)
+            return result
+
+        if len(right) == 0:
+            result.extend(left)
+            return result
+
+        if left[0] > right[0]:
+            result.append(right.pop(0))
+        else:
+            result.append(left.pop(0))
+
+
+    # for n in range(len(l)):
+    #     halva = len(l)/2
+    #     Första_halvan = [:halva]
+    #     Andra_halvan = [halva:]
+    #
+    #
+    #     if l[0]
+    #
+    # return l
